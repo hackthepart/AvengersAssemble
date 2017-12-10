32ef0527e847ff99f2ef057ffc6e7ed2a184b549
Divyanshu Kapoor <34399087+divyanshukapoor@users.noreply.github.com>
2017-12-10 20:36:02 +0530

    Create hello_world.cpp

---
+++ scripts/hello_world.cpp
@@ -0,0 +1,15 @@
+/*
+	LANGUAGE: CPP
+	AUTHOR: Divyanshu Kapoor
+	GITHUB: https://github.com/divyanshukapoor
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	cout<<"Hello MNNIT!"<<endl;
+
+
+}
