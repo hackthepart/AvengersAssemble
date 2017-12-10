978f5bb7f7b85c3ae59846bd9d1d629dc3eb75b2
anisha kumari <kinshukri1996@gmail.com>
2017-12-10 23:19:33 +0530

    Added my hello world code

---
+++ scripts/hello_world.cpp
@@ -0,0 +1,12 @@
+//language: c++
+//author : ANISHA KUMARI
+//github:  https://github.com/anishakinshu
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "HELLO , WORLD!" << "\n";
+    return 0;
+}
