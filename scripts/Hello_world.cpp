40b087efcee9fc7ea3243754dc1c1f917527add5
kartik mahendru <mahendru.kartik@gmail.com>
2017-12-10 16:35:09 +0530

    Files added to scripts

---
+++ scripts/Hello_world.cpp
@@ -0,0 +1,12 @@
+/*
+LANGUAGE: C++
+AUTHOR:         Kartik Mahendru
+GITHUB:         https://github.com/kartikMahendru
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+cout << "Hello MNNIT!" << endl;
+return 0;
+}
---
+++ scripts/Hello_world.py
@@ -0,0 +1,7 @@
+'''
+LANGUAGE: python
+AUTHOR:		Kartik Mahendru
+GITHUB:		https://github.com/kartikMahendru
+'''
+print('Hello MNNIT!')
+
