ebdc35afe2df6dc2405da145e82968f877c25a20
Abhey Rana <abheyranacool123.ar@gmail.com>
2017-12-11 16:17:58 +0530

    * Indented the list items properly and added missing </a> tags
    * Added name in CONTRIBUTORS.md
    
    * Added signature in index.html file
    
    * Added Abhey_Rana.md in profiles section
    
    * Added hello_world.go
    
    * Indented the list items properly and added missing </a> tags

---
+++ scripts/hello_world.go
@@ -0,0 +1,17 @@
+/*
+	LANGUAGE: GO
+	AUTHOR: Abhey Rana
+	GITHUB: https://github.com/Abhey
+	TUTORIAL LINK: https://www.golang-book.com/books/intro/2
+*/
+
+package main
+
+// import fmt(formatting) package responsible for formatting of input and output.
+import "fmt"
+
+func main() {
+	
+	fmt.Println("Hello MNNIT!")
+
+}
