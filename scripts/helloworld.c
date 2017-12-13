af4422aa3816ecbbef9dc161829767974177c56d
Sahaj Bamba <30756602+Sahaj-Bamba@users.noreply.github.com>
2017-12-13 11:41:29 -0800

    Create helloworld.c
    hello world without semi colon

---
+++ scripts/helloworld.c
@@ -0,0 +1,10 @@
+// Language : C
+// Author   : Sahaj Bamba
+// Github   : https://github.com/Sahaj-Bamba
+// hello world without semicolon
+
+#include<stdio.h>
+int main()
+{
+  if(print("Hello MNNIT!")) {}
+}
