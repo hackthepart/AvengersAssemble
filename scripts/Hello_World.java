d4608553d44a7d1bf27fc48adbdd9a43e7785125
descifrado <34531141+descifrado@users.noreply.github.com>
2017-12-15 11:02:35 +0530

    Create Hello_World.java

---
+++ scripts/Hello_World.java
@@ -0,0 +1,13 @@
+/*
+ LANGUAGE: java
+ AUTHOR  : [ANUJ MODI](https://github.com/descifrado)
+ GITHUB  : https://github.com/descifrado
+*/
+
+public class hello
+{
+public static void main(String args[])
+{
+System.out.println("Hello WORLD!");
+}
+}
