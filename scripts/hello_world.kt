cd6ce1b228a085db6556dcd54e9002b6c3afd36b
Abhishek Kumar <abhi1358.mnnit@gmail.com>
2017-12-16 15:15:08 +0530

    Create hello_world.kt

---
+++ scripts/hello_world.kt
@@ -0,0 +1,11 @@
+# LANGUAGE: Kotlin
+# AUTHOR: Abhishek Kumar
+# GITHUB: https://github.com/abhi1358
+
+import java.util.*
+ 
+// Hello World Program
+ 
+fun main(args : Array<String>) {
+    println("Hello, World!")
+}
