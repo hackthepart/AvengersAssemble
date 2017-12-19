33fb54e2ba7fcf3a7a9422ac414104bc7a442ca7
pranaygoud36 <pranaygoud36@gmail.com>
2017-12-19 22:40:26 +0530

    #Updated files

---
+++ scripts/hello_world.sh
@@ -0,0 +1,14 @@
+# LANGUAGE: bash
+# AUTHOR:		Jatin Rungta
+# GITHUB:		https://github.com/urdarinda
+
+#!/bin/bash
+echo "Hello MNNIT!"
+
+
+# LANGUAGE: bash
+# AUTHOR:		Rajan Jha
+# GITHUB:		https://github.com/iamrajanjharj
+
+#!/bin/bash
+echo "Hello World"
