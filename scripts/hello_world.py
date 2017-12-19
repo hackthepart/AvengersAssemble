6a991706704caeebcee19146df0b720a9339f7dc
pranaygoud36 <pranaygoud36@gmail.com>
2017-12-19 22:31:14 +0530

    #Updated files

--- scripts/hello_world.py
+++
@@ -1,9 +0,0 @@
-"""
-
-	# LANGUAGE: Python
-	# AUTHOR:		Pranay kumar
-	# GITHUB:		https://github.com/pranaykumargoud	
-	
-"""
-
-print("Hello Ex!!")
--- scripts/hello_world.sh
+++
@@ -1,6 +0,0 @@
-# LANGUAGE: bash
-# AUTHOR:		Pranay kumar
-# GITHUB:		https://github.com/pranaykumargoud
-
-#!/bin/bash
-echo "Hello MNNIT!"
