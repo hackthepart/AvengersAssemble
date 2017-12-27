40baa04492bea8a3881963a2eb8c47ad4ff05177
Khitk <akshitsharma2811>
2017-12-27 22:27:14 +0530

    added by akshit

---
+++ scripts/Hello_World.cfm
@@ -0,0 +1,6 @@
+<!--- 
+LANGUAGE: ColdFusion
+AUTHOR: Akshit Sharma
+GITHUB: https://github.com/khitk9738 
+--->
+<cfoutput>Hello, World!</cfoutput>
---
+++ scripts/Hello_World.hs
@@ -0,0 +1,7 @@
+-- LANGUAGE: Haskell
+-- AUTHOR: Akshit Sharma
+-- GITHUB: https://github.com/khitk9738
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, World!"
